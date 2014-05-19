49138cac96e7665869bcafd50f09dd0e0672454a
Akihiko Kato <imaginator01@gmail.com>
2014-05-20 00:03:30 +0900

    unicode対応

--- set_002/set_002_16.py
+++ set_002/set_002_16.py
@@ -4,11 +4,11 @@
 import sys
 import re
 
-ptn = re.compile(r'([一-龠]+)\(([A-Z]+)\)',re.UNICODE)
+ptn = re.compile(ur'([一-龠]+)\(([A-Z]+)\)',re.UNICODE)
 
 lines = sys.stdin.readlines()
 for line in lines:
-	for elem in ptn.findall(line):
+	for elem in ptn.findall(unicode(line,"utf-8")):
 		print elem[0],"\t",elem[1]
 
 
